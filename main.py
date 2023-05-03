07706bf1b09bdc7d8a3b3a7c6ccd277ea7f4cdd7
jesunglim <jesunglimkorea@gmail.com>
2023-05-03 16:13:20 +0900

    multi-lang index

--- main.py
+++ main.py
@@ -1,73 +1,48 @@
+from utils.html import *
+
 import googletrans
+
 
 trans = googletrans.Translator()
 
 
-html_start = '''<!DOCTYPE html>\n
+html_start = '''<!DOCTYPE html>\n<html>\n<head>\n'''
+html_end = '''\n</body>\n</html>\n'''
+
+html_start_lang = '''<!DOCTYPE html>\n
 <html>\n
     <head>\n
         <meta charset="utf-8">\n
         <title>Title</title>\n
 <link rel="stylesheet" href="theme.css">\n
     </head>\n
-	<body>\n'''
-html_end = '''\n    </body>\n</html>\n'''
+	<body>\n
+'''
 
-def merge_html(content):
-    return html_start + "".join(content) + html_end
+
+
+def index_html_lang(languages):
+    index_str = ""
+    index_str += '  <meta http-equiv="content-language" content="kr">\n'
+    index_str += '  <meta http-equiv="refresh" content="0; URL=lang/kr.html">\n'
+
+    for i in languages:
+        index_str += '  <meta http-equiv="content-language" content="' + i +  '">\n'
+        index_str += '  <meta http-equiv="refresh" content="0; URL=lang/' + i + '.html">\n'
+    index_str += '''   <title>My Website</title>\n</head>\n<body>\n'''
+    return index_str
+
+def merge_html(languages):
+    return html_start + index_html_lang(languages) + html_end
+
+def merge_html_lang(content):
+    return html_start_lang + "".join(content) + html_end
 
 def t(word, lang):
     if lang == 'ko':
         return word
     
     return (trans.translate(word, dest=lang)).text
-
-def br():
-    return "<br>\n"
-
-def p(word):
-    start = "   <p>"
-    end = "</p>\n"
-    return start+word+end
-
-def h1(word):
-    start = "   <h1>"
-    end  = "</h1>\n"
-    return start+word+end
-
-def h1(word):
-    start = "   <h1>"
-    end  = "</h1>\n"
-    return start+word+end
-
-def h2(word):
-    start = "   <h2>"
-    end  = "</h2>\n"
-    return start+word+end
-
-def h3(word):
-    start = "   <h3>"
-    end  = "</h3>\n"
-    return start+word+end
-
-def h4(word):
-    start = "   <h4>"
-    end  = "</h4>\n"
-    return start+word+end
-
-def h5(word):
-    start = "   <h5>"
-    end  = "</h5>\n"
-    return start+word+end
-
-def h6(word):
-    start = "   <h6>"
-    end  = "</h6>\n"
-    return start+word+end
-
-
-
-
 
 
 
@@ -98,16 +73,18 @@ def editor(lang):
 lang_list = ['en', 'ja', 'zh-cn', 'zh-tw']
 
 def main():
-    page = merge_html(editor('ko'))
+    f = open('index.html', 'w')
+    f.write(merge_html(lang_list))
+    f.close()
     
     f = open('lang/kr.html', 'w')
-    f.write(page)
+    f.write(merge_html_lang(editor('ko')))
     f.close()
 
-    for l in lang_list:
-        trans_page = merge_html(editor(l))
+    for i in lang_list:
+        trans_page = merge_html_lang(editor(l))
 
-        f = open(f'lang/{l}.html', 'w')
+        f = open(f'lang/{i}.html', 'w')
         f.write(trans_page)
         f.close()
 
---
+++ utils/html.py
@@ -0,0 +1,45 @@
+
+def br():
+    return "<br>\n"
+
+def p(word):
+    start = "   <p>"
+    end = "</p>\n"
+    return start+word+end
+
+def h1(word):
+    start = "   <h1>"
+    end  = "</h1>\n"
+    return start+word+end
+
+def h1(word):
+    start = "   <h1>"
+    end  = "</h1>\n"
+    return start+word+end
+
+def h2(word):
+    start = "   <h2>"
+    end  = "</h2>\n"
+    return start+word+end
+
+def h3(word):
+    start = "   <h3>"
+    end  = "</h3>\n"
+    return start+word+end
+
+def h4(word):
+    start = "   <h4>"
+    end  = "</h4>\n"
+    return start+word+end
+
+def h5(word):
+    start = "   <h5>"
+    end  = "</h5>\n"
+    return start+word+end
+
+def h6(word):
+    start = "   <h6>"
+    end  = "</h6>\n"
+    return start+word+end
+
+
